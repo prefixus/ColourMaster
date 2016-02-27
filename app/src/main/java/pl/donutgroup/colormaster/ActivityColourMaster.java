244624ab80ff3c0e096e557a0964a6dd3d7c26a3
Prefixus <rafpra@gmail.com>
2016-02-27 13:46:06 +0100

    Initial commit

---
+++ app/src/main/java/pl/donutgroup/colormaster/ActivityColourMaster.java
@@ -0,0 +1,13 @@
+package pl.donutgroup.colormaster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityColourMaster extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colour_master);
+    }
+}
