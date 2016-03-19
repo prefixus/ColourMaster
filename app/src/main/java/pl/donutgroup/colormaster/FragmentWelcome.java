deecfdce3a753871040b049a7700dafd70ccc790
Rafał Harakop <rafal.harakop@gmail.com>
2016-03-19 19:34:09 +0100

    issue/4 First version of select color game screen

--- app/src/main/java/pl/donutgroup/colormaster/FragmentWelcome.java
+++ app/src/main/java/pl/donutgroup/colormaster/FragmentWelcome.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * Created by Kasia on 2016-02-27.
@@ -13,6 +14,12 @@ public class FragmentWelcome extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View v = inflater.inflate(R.layout.fragment_welcome, null);
+		((Button)v.findViewById(R.id.play)).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				((ActivityColorMaster)getActivity()).loadFragment(FragmentGameSelectColor.getInstance());
+			}
+		});
 		return v;
 	}
 
